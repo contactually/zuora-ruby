64986c9f2c69094f2d53577f2678b41c834b8afb
Shaun Robinson <shaun@contactually.com>
2016-01-11 15:43:24 -0500

    Adds base resource functionality

---
+++ lib/zuora/resources.rb
@@ -0,0 +1,17 @@
+module Zuora
+  module Resources
+    InvalidModel = Class.new(StandardError)
+
+    def self.with_valid(model, &block)
+      if model.valid?
+        block.call model
+      else
+        raise InvalidModel.new(model)
+      end
+    end
+  end
+end
+
+require_relative 'resources/accounts'
+require_relative 'resources/payment_methods'
+require_relative 'resources/subscriptions'
