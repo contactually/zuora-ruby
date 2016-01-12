48ce52c33ccec0a81829e1096e85c5317191106f
Shaun Robinson <shaun@contactually.com>
2016-01-12 16:08:31 -0500

    Dirty tracking first pass

--- lib/zuora/models/contact.rb
+++ lib/zuora/models/contact.rb
@@ -1,35 +1,32 @@
-# encoding: utf-8
+require_relative '../models/utils/dirty'
+require 'active_support'
 
 module Zuora
   module Models
     class Contact
-      include ActiveModel::Model
+      include ActiveModel::Validations
+      include ActiveModel::Dirty
+      include DirtyAttrAccessor
 
-      ATTRIBUTES = :address_1,
-                   :address_2,
-                   :city,
-                   :country,
-                   :county,
-                   :fax,
-                   :first_name,
-                   :home_phone,
-                   :last_name,
-                   :mobile_phone,
-                   :nickname,
-                   :other_phone,
-                   :other_phone_type,
-                   :personal_email,
-                   :zip_code,
-                   :state,
-                   :tax_region,
-                   :work_email,
-                   :work_phone
-
-      attr_accessor(*ATTRIBUTES)
-
-      def attributes
-        ATTRIBUTES
-      end
+      dirty_attr_accessor :address_1,
+                          :address_2,
+                          :city,
+                          :country,
+                          :county,
+                          :fax,
+                          :first_name,
+                          :home_phone,
+                          :last_name,
+                          :mobile_phone,
+                          :nickname,
+                          :other_phone,
+                          :other_phone_type,
+                          :personal_email,
+                          :zip_code,
+                          :state,
+                          :tax_region,
+                          :work_email,
+                          :work_phone
 
       validates :first_name,
                 :last_name,
@@ -39,6 +36,12 @@ module Zuora
       validates :first_name,
                 :last_name,
                 length: { maximum: 100 }
+
+      def initialize(attrs = {})
+        attrs.each do |attr, val|
+          instance_variable_set "@#{attr}", val
+        end
+      end
     end
   end
 end
