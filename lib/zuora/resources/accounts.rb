dfd88643d4a246717bb56ff312a8d2a0e90c9c2e
Shaun Robinson <shaun@contactually.com>
2016-01-14 13:27:16 -0500

    Remove validation logic from resource (models are always valid)

--- lib/zuora/resources/accounts.rb
+++ lib/zuora/resources/accounts.rb
@@ -4,15 +4,11 @@ module Zuora
       RESOURCE_URI = '/rest/v1/accounts'
 
       def self.create!(client, model, serializer = Zuora::Serializers::Noop)
-        Zuora::Resources.with_valid model do |mod|
-          client.post RESOURCE_URI, serializer.serialize(mod)
-        end
+        client.post RESOURCE_URI, serializer.serialize(model)
       end
 
       def self.update!(client, model, serializer = Zuora::Serializers::Noop)
-        Zuora::Resources.with_valid model do |mod|
-          client.post RESOURCE_URI, serializer.serialize(mod)
-        end
+        client.post RESOURCE_URI, serializer.serialize(model)
       end
     end
   end
