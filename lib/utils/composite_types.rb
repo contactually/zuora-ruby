f1d1530da0f3512368174a9ec2538ddd61516040
John Gerhardt <jgerhardt@contactually.com>
2016-01-21 15:41:53 -0500

    Removes booleans class and type

--- lib/utils/composite_types.rb
+++
@@ -1,6 +0,0 @@
-require 'date'
-
-module Boolean; end
-
-class TrueClass; include Boolean; end
-class FalseClass; include Boolean; end
--- lib/utils/schema_model.rb
+++ lib/utils/schema_model.rb
@@ -1,4 +1,3 @@
-require_relative 'composite_types'
 require 'byebug'
 
 module SchemaModel
